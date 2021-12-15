91026d531d7d4aef9b4601d1c66418179beca24e
Lucas-JAOUEN <lucas.jaouen1@gmail.com>
2021-12-15 20:08:10 +0100

    responsive navbar

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,71 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <ResponsiveNav
+		:nav-links="navLinks"
+		background="#fff"
+		link-color="#777"
+		hoverBackground="#ddd"
+    />
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
+<script>
+import ResponsiveNav from '@/components/ResponsiveNav.vue'
+export default {
+  components: {
+    ResponsiveNav
+  },
+  data: () => ({
+    navLinks: [
+      {
+        text: 'Contact',
+        path: '/contact',
+        icon: 'ion-ios-megaphone'
+      },
+      {
+        text: 'About',
+        path: '/about',
+        icon: 'ion-ios-business'
+      },
+      {
+        text: 'Blog',
+		path: '/blog',
+		icon: 'ion-ios-bonfire'
+      },
+      {
+        text: 'Portfolio',
+        path: '/portfolio',
+        icon: 'ion-ios-briefcase'
+      }
+    ]
+  })
+}
+</script>
+
 <style lang="scss">
+@import 'https://unpkg.com/ionicons@4.2.2/dist/css/ionicons.min.css';
+figure {
+	margin-block-start: 0;
+	margin-block-end: 0;
+	margin-inline-start: 10px;
+	margin-inline-end: 0;
+}
+body {
+  margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
-
     &.router-link-exact-active {
       color: #42b983;
     }
