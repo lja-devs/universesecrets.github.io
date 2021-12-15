625bbca078b5f2a0ddf3859ed2fca7619adda016
Lucas-JAOUEN <lucas.jaouen1@gmail.com>
2021-12-15 22:01:46 +0100

    New Deployment

---
+++ precache-manifest.cfe52f3bfb3e10943d478528111801e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fefaf6be6d9af05416ab",
+    "url": "/universesecrets.github.io/css/app.9541507b.css"
+  },
+  {
+    "revision": "f422536d9767f797cb5c110820368e89",
+    "url": "/universesecrets.github.io/index.html"
+  },
+  {
+    "revision": "f91553c317f2bbb0d4fc",
+    "url": "/universesecrets.github.io/js/about.4205823e.js"
+  },
+  {
+    "revision": "fefaf6be6d9af05416ab",
+    "url": "/universesecrets.github.io/js/app.b33e45aa.js"
+  },
+  {
+    "revision": "9f1ddcd77b4230387d3f",
+    "url": "/universesecrets.github.io/js/chunk-vendors.6016a256.js"
+  },
+  {
+    "revision": "0098001ad328d149a066bb2cb3df981c",
+    "url": "/universesecrets.github.io/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/universesecrets.github.io/robots.txt"
+  }
+]);
